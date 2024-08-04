cd80f7df36053abe649d10e7415b59fefd18ff24
Woojin Choi <dnwls010728@gmail.com>
2024-08-04 15:30:20 +0900

    FEAT: Character 관련 추가

---
+++ Source/ProjectH/Private/Player/MyPlayerController.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/MyPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+AMyPlayerController::AMyPlayerController()
+{
+	ASC = nullptr;
+}
+
+void AMyPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(InputContext);
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(InputContext, 0);
+	
+}
+
+void AMyPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+}
---
+++ Source/ProjectH/Public/Character/MyCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/MyCharacterBase.h"
+#include "MyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTH_API AMyCharacter : public AMyCharacterBase
+{
+	GENERATED_BODY()
+	
+};

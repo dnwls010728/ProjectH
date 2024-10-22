d2d2182253ad73085f99090aa15d24fe214c758d
Hospiea <155417815+Hospiea@users.noreply.github.com>
2024-10-22 16:59:04 +0900

    FIX: Cache 정리

--- Source/ProjectH/Private/Objects/InteractableObject.cpp
+++ Source/ProjectH/Private/Objects/InteractableObject.cpp
@@ -12,7 +12,7 @@ AInteractableObject::AInteractableObject()
 	PrimaryActorTick.bCanEverTick = false;
 	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
 	RootComponent = Capsule;
-
+	
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
 }
 
@@ -29,11 +29,11 @@ void AInteractableObject::BeginPlay()
 void AInteractableObject::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	
 }
 
 void AInteractableObject::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
-
+	
 }
 
 void AInteractableObject::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) {
